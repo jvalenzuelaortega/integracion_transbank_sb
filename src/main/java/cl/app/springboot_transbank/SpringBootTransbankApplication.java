af02bedc0d3ede605bc4d03e3ec934fb8c880e97
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2020-08-09 21:12:44 -0400

    Codigo base. Se agrega estructura nueva del anterior proyecto (integrador_pagos_web)

---
+++ src/main/java/cl/app/springboot_transbank/SpringBootTransbankApplication.java
@@ -0,0 +1,13 @@
+package cl.app.springboot_transbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransbankApplication.class, args);
+	}
+
+}
