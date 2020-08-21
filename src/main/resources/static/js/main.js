9f93c3beac7c4a009598a5f73b0b532450b84a66
Juan Valenzuela <jvalenzuelaortega@gmail.com>
2020-08-21 00:18:55 -0400

    Logica de checkbox para pasar a la segunda vista

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,25 @@
 //JS principal
-var amountDefault = 1000;
-var mySession = "mi-id-de-sesion";
-var buyOrder = Math.floor((Math.random() * 1000000000) + 1);
+$(document).ready(function () {
+	var amountDefault = 1000;
+	var mySession = "mi-id-de-sesion";
+	var buyOrder = Math.floor((Math.random() * 1000000000) + 1);
+	
+	$('#sessionIdInpt').val(mySession);
+	$('#buyOrderInpt').val(buyOrder);
+	$('#montoInpt').val(amountDefault);
+	
+	$('#comprarBtn').prop('disabled', true);
+	
+	$('#checkBuy').click(function() {
+		if($("#checkBuy").is(':checked')){
+		    $('#comprarBtn').prop('disabled', false);
+		}else{
+		    $('#comprarBtn').prop('disabled', true);
+		}
+	});
+	
+	
+})
 
-$('#sessionIdInpt').val(mySession);
-$('#buyOrderInpt').val(buyOrder);
-$('#montoInpt').val(amountDefault);
+
 
