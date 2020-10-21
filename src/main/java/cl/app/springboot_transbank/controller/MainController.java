b8c5aca3114be805635719cbcef58f6cece966d0
jvalenzuelaortega <46851455+jvalenzuelaortega@users.noreply.github.com>
2020-10-21 16:13:01 -0300

    Pruebas de LOG
    Se integra LOG4J2 y se hace prueba rapida de logs con la libreria

--- src/main/java/cl/app/springboot_transbank/controller/MainController.java
+++ src/main/java/cl/app/springboot_transbank/controller/MainController.java
@@ -1,5 +1,7 @@
 package cl.app.springboot_transbank.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,10 +11,17 @@ import cl.app.springboot_transbank.model.Purchase;
 @Controller
 public class MainController {
 
+	private static final Logger LOG = LogManager.getLogger(MainController.class);
+
 	@GetMapping("/")
 	public ModelAndView getIndex() {
 		ModelAndView mav = new ModelAndView("index");
 		mav.addObject("purchase", new Purchase());
+		LOG.debug("Debugging log in our greeting method");
+		LOG.info("Info log in our greeting method");
+		LOG.warn("Warning log in our greeting method");
+		LOG.error("Error in our greeting method");
+		LOG.fatal("Damn! Fatal error. Please fix me.");
 		return mav;
 	}
 }
